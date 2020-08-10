a5ab6cc2d4a388e65b302f40af719396174662e2
Emre Çakır <ceamkrier@gmail.com>
2020-08-10 22:16:03 +0300

    multi-container-app: removed unused import from client

--- multi-container-app/client/src/fib.js
+++ multi-container-app/client/src/fib.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
 
 export const Fib = () => {
